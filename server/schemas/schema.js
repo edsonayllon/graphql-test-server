885d9e61935bd83ef6f91b650040d2a4364f6f10
eayllon1 <eayllon1@gmail.com>
2019-05-27 13:26:04 -0400

    added type id

--- server/schemas/schema.js
+++ server/schemas/schema.js
@@ -1,5 +1,10 @@
 const graphql = require('graphql');
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLID
+} = graphql;
 const _ = require('lodash');
 
 // dummy data
@@ -9,10 +14,16 @@ var books = [
     { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
 ];
 
+var authors = [
+    { name: 'Patrick Rothfuss', age: 44, id: '1' },
+    { name: 'Brandon Sanderson', age: 42, id: '2' },
+    { name: 'Terry Pratchett', age: 66, id: '3' }
+];
+
 const BookType = new GraphQLObjectType({
   name: 'Book',
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString }
   })
@@ -23,7 +34,7 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     book: {
       type: BookType,
-      args: { id: { type: GraphQLString } },
+      args: { id: { type: GraphQLID } },
       resolve(parent, args){
         // code to get data from db
         return _.find(books, { id: args.id });
